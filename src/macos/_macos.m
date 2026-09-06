3a76e10db8c51efca3afd12c66025e2dc155043f
Ricci Adams <git@ricciadams.com>
2026-09-06 11:36:47 -0700

    macOS native version of subplot tool

--- src/macos/_macos.m
+++ src/macos/_macos.m
@@ -5,6 +5,7 @@
 #import "MPLFigureCanvas.h"
 #import "MPLFigureManager.h"
 #import "MPLNavigationToolbar2.h"
+#import "MPLSubplotTool.h"
 
 #define PY_SSIZE_T_CLEAN
 #import <Python.h>
@@ -928,6 +929,120 @@ static PyTypeObject TimerType = {
 };
 
 
+#pragma mark - Subplot Controller
+
+typedef struct {
+    PyObject_HEAD
+    __strong MPLSubplotTool *object;
+} SubplotTool;
+
+static PyObject *
+SubplotTool_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    return (PyObject *)((SubplotTool *)type->tp_alloc(type, 0));
+}
+
+static int
+SubplotTool_init(SubplotTool *self, PyObject *args, PyObject *kwds)
+{
+    BEGIN_OBJC_ENTRY
+
+    PyObject *figureManagerPyObject;
+    if (!PyArg_ParseTuple(args, "O", &figureManagerPyObject)) {
+        return -1;
+    }
+
+    MPLFigureManager *figureManager = ((FigureManager *)figureManagerPyObject)->object;
+
+    self->object = [[MPLSubplotTool alloc] initWithFigureManager:figureManager];
+    [self->object setPyObject:(PyObject *)self];
+
+    END_OBJC_ENTRY
+    return 0;
+}
+
+static void
+SubplotTool_dealloc(SubplotTool *self)
+{
+    BEGIN_OBJC_ENTRY
+    [self->object setPyObject:NULL];
+    self->object = nil;
+    END_OBJC_ENTRY
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *
+SubplotTool_repr(SubplotTool *self)
+{
+    return PyUnicode_FromFormat("SubplotTool<%p> wrapping MPLSubplotTool<%p>",
+                                (void *)self, (__bridge void *)self->object);
+}
+
+static PyObject *
+SubplotTool__send_params_to_ui(SubplotTool *self, PyObject *args)
+{
+    BEGIN_OBJC_ENTRY
+
+    double left, bottom, right, top, wspace, hspace;
+    if (!PyArg_ParseTuple(args, "dddddd", &left, &bottom, &right,
+                          &top, &wspace, &hspace)) { return NULL; }
+
+    [self->object updateWithLeft: left
+                          bottom: bottom
+                           right: right
+                             top: top
+                          wspace: wspace
+                          hspace: hspace];
+
+    END_OBJC_ENTRY
+    RETURN_NULL_OR_NONE
+}
+
+static PyObject *
+SubplotTool_show(SubplotTool *self, PyObject *args)
+{
+    BEGIN_OBJC_ENTRY
+    [self->object showWindow:self->object];
+    END_OBJC_ENTRY
+    RETURN_NULL_OR_NONE
+}
+
+static PyObject *
+SubplotTool_close(SubplotTool *self, PyObject *args)
+{
+    BEGIN_OBJC_ENTRY
+    [self->object close];
+    END_OBJC_ENTRY
+    RETURN_NULL_OR_NONE
+}
+
+static PyTypeObject SubplotToolType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "matplotlib.backends._macosx.SubplotTool",
+    .tp_doc = PyDoc_STR(""),
+    .tp_basicsize = sizeof(SubplotTool),
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+
+    .tp_new = (newfunc)SubplotTool_new,
+    .tp_init = (initproc)SubplotTool_init,
+    .tp_dealloc = (destructor)SubplotTool_dealloc,
+    .tp_repr = (reprfunc)SubplotTool_repr,
+
+    .tp_methods = (PyMethodDef[]){  // All docstrings are inherited.
+        {"_send_params_to_ui",
+         (PyCFunction)SubplotTool__send_params_to_ui,
+         METH_VARARGS},
+        {"close",
+         (PyCFunction)SubplotTool_close,
+         METH_NOARGS},
+        {"show",
+         (PyCFunction)SubplotTool_show,
+         METH_NOARGS},
+        {}  // sentinel
+    },
+};
+
+
 #pragma mark - Module
 
 static PyObject *
@@ -1096,7 +1211,8 @@ _macos_mod_exec(PyObject *m)
     if (PyModule_AddType(m, &FigureCanvasType)
         || PyModule_AddType(m, &FigureManagerType)
         || PyModule_AddType(m, &NavigationToolbar2Type)
-        || PyModule_AddType(m, &TimerType)) {
+        || PyModule_AddType(m, &TimerType)
+        || PyModule_AddType(m, &SubplotToolType)) {
         return -1;
     }
     return 0;
